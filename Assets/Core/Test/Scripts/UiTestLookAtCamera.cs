372df2bd47e98325592291cdab4837272ea91e35
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-02-25 10:38:17 +0100

    test ui 2

--- Assets/Core/Test/Scripts/UiTestLookAtCamera.cs
+++ Assets/Core/Test/Scripts/UiTestLookAtCamera.cs
@@ -4,16 +4,32 @@ namespace Core.Test.Scripts
 {
     public class UiTestLookAtCamera : MonoBehaviour
     {
-        private Camera cameraMain;
+        private Camera mainCamera;
 
-        private void Awake()
+        void Start()
         {
-            cameraMain = Camera.main;
+            mainCamera = Camera.main;
         }
 
-        private void LateUpdate()
+        void LateUpdate()
         {
-            transform.LookAt(transform.position + Camera.main!.transform.rotation * Vector3.forward, cameraMain.transform.rotation * Vector3.up);
+            // Récupérer la position de la caméra et du canvas
+            Vector3 cameraPosition = mainCamera.transform.position;
+            Vector3 canvasPosition = transform.position;
+
+            // Créer un vecteur direction entre la caméra et le canvas
+            Vector3 direction = cameraPosition - canvasPosition;
+
+            // Calculer la nouvelle rotation du canvas en utilisant la direction entre la caméra et le canvas
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            targetRotation.eulerAngles = new Vector3(0f, targetRotation.eulerAngles.y, 0f);
+
+            // Appliquer la rotation au canvas
+            transform.rotation = targetRotation;
+            
+            // Corriger l'orientation à 180 degrés
+            Quaternion currentRotation = transform.rotation;
+            transform.rotation = currentRotation * Quaternion.Euler(0f, 180f, 0f);
         }
     }
 }
---
+++ Assets/Core/Test/Scripts/UiTestLookAtCamera2.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Core.Test.Scripts
+{
+    public class UiTestLookAtCamera2 : MonoBehaviour
+    {
+        private Camera cameraMain;
+
+        private void Awake()
+        {
+            cameraMain = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            transform.LookAt(transform.position + Camera.main!.transform.rotation * Vector3.forward, cameraMain.transform.rotation * Vector3.up);
+        }
+    }
+}
