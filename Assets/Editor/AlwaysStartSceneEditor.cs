27e9eb41d439f7cac8dc3057acf87d4279ed7d3c
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-05-06 11:50:27 +0200

    refactor AlwaysStartSceneEditor

--- Assets/Editor/AlwaysStartSceneEditor.cs
+++ Assets/Editor/AlwaysStartSceneEditor.cs
@@ -16,20 +16,38 @@ public class AlwaysStartSceneEditor
 
     private static void PlayModeStateChanged(PlayModeStateChange state)
     {
-        if (!state.Equals(PlayModeStateChange.ExitingEditMode)) return;
-        _previousScenePath = SceneManager.GetActiveScene().path;
+        if (!state.Equals(PlayModeStateChange.ExitingEditMode))
+        {
+            if (state.Equals(PlayModeStateChange.EnteredEditMode))
+            {
+                CleanupLoadedScenes();
+            }
+        }
+        else
+        {
+            _previousScenePath = SceneManager.GetActiveScene().path;
 
-        if (_previousScenePath == StartScenePath) return;
-        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
-        EditorSceneManager.OpenScene(StartScenePath, OpenSceneMode.Single);
+            if (_previousScenePath.Equals(StartScenePath)) return;
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+            EditorSceneManager.OpenScene(StartScenePath, OpenSceneMode.Single);
+        }
     }
 
     private static void OnSceneOpened(Scene scene, OpenSceneMode mode)
     {
-        if (mode != OpenSceneMode.Single || scene.path != StartScenePath || string.IsNullOrEmpty(_previousScenePath) || EditorApplication.isPlaying) return;
-        
+        if (!mode.Equals(OpenSceneMode.Single) || !scene.path.Equals(StartScenePath) || string.IsNullOrEmpty(_previousScenePath) || EditorApplication.isPlaying) return;
+
         EditorSceneManager.OpenScene(_previousScenePath, OpenSceneMode.Additive);
         SceneManager.SetActiveScene(SceneManager.GetSceneByPath(_previousScenePath));
         EditorSceneManager.sceneOpened -= OnSceneOpened;
+    }
+
+    private static void CleanupLoadedScenes()
+    {
+        var persistentScene = SceneManager.GetSceneByPath(StartScenePath);
+        if (persistentScene.IsValid() && persistentScene.isLoaded)
+        {
+            EditorSceneManager.CloseScene(persistentScene, true);
+        }
     }
 }
